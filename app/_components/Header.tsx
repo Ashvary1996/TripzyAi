57f655b6a1fd867d89261dca5d119a71f3c715c3
Ashvary1996 <ashvarygidian1996@gmail.com>
2025-08-29 15:26:31 +0530

    shadcn + header added

---
+++ app/_components/Header.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+  const menuOptions = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Pricing",
+      path: "/pricing",
+    },
+    {
+      name: "Contact Us",
+      path: "/contact-us",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center p-4">
+      {/* //Logo// */}
+      <div className="flex gap-2 items-center">
+        <Image src={"/logo.svg"} alt="logo" width={30} height={30} />
+        <h2 className="font-bold text-2xl ">Tripzy-Ai</h2>
+      </div>
+      {/* Menu Options */}
+      <div className="flex gap-5 items-center">
+        {menuOptions.map((menu) => {
+          return (
+            <Link href={menu.path}>
+              <h2 className="text-lg hover:scale-105 transition-all hover:text-primary">{menu.name}</h2>
+            </Link>
+          );
+        })}
+      </div>
+      {/* Get Started Button */}
+      <div>
+        <Button>
+            Get Started
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='mt-2 flex items-center'>
+        {/* Content */}
+        {/* Input Box */}
+        {/* Suggestions */}
+    </div>
+  )
+}
+
+export default Hero
